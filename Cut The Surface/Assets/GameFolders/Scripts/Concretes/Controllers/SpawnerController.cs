bf6798e3d35e0785ee31b84d04ca37692237f293
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2022-09-07 21:26:16 +0300

    SpawnerController eklendi

--- Cut The Surface/Assets/GameFolders/Scripts/Concretes/Controllers/SpawnerController.cs
+++ Cut The Surface/Assets/GameFolders/Scripts/Concretes/Controllers/SpawnerController.cs
@@ -1,18 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class SpawnerController : MonoBehaviour
+
+namespace CutTheSurface.Controllers
 {
-    // Start is called before the first frame update
-    void Start()
+    public class SpawnerController : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private EnemyController _enemyprefab;
+        [Range(0.1f,5f)][SerializeField] float _min=0.1f;
+        [Range(6f, 15f)] [SerializeField] private float _max = 15f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        float _maxSpawnTime;
+        float _currentSpawnTime=0f;
+
+        private void OnEnable()
+        {
+            GetRandomMaxTime();
+        }
+
+        void Update()
+        {
+            _currentSpawnTime += Time.deltaTime;
+            if (_currentSpawnTime > _maxSpawnTime)
+            {
+                Spawn();
+            }
+        }
+
+        void Spawn()
+        {
+            EnemyController newEnemy=Instantiate(_enemyprefab, transform.position, transform.rotation);
+            newEnemy.transform.parent = this.transform;
+
+            _currentSpawnTime = 0f;
+            GetRandomMaxTime();
+        }
+
+        private void GetRandomMaxTime()
+        {
+            _maxSpawnTime = Random.Range(_min, _max);
+        }
     }
+    
 }
+
+
